6e737936cefc55f1ace41ae282631cc4b3ab0609
Shaswat <shaswatrajput7@gmail.com>
2023-12-14 19:40:18 +0530

    changed default route message

--- backend/app.js
+++ backend/app.js
@@ -11,14 +11,14 @@ const router = require("./routes/weather-routes")
     res.status(200).json({
         status:"successful",
         greeting: "Hello welcome to the weather app",
-        routes: ` "/" - Default Home Route
-        "/locations" - ( GET ) - to get all the location
-        "/locations" - ( POST ) - to add a new location ({ name, latitude, longitude})
-        "/locations/:id" - ( GET ) - to get a saved location by ID
-        "/locations/:id" - ( PUT ) - to update a saved location by ID
-        "/locations/:id" - ( DELETE ) - to delete a saved location by ID
-        "/history/:id/:date" - ( GET ) - to get history weather of a location by ID amd Date
-        "/weatherByName/:name" - ( GET ) - to get weather data by simply entering name of the city as params `  
+        route1: `"/" - Default Home Route`,
+        route2:`"/locations" - ( GET ) - to get all the location`,
+        route3:`"/locations" - ( POST ) - to add a new location ({ name, latitude, longitude})`,
+        route4:`"/locations/:id" - ( GET ) - to get a saved location by ID`,
+        route5:`"/locations/:id" - ( PUT ) - to update a saved location by ID`,
+        route6:`"/locations/:id" - ( DELETE ) - to delete a saved location by ID`,
+        route7:`"/history/:id/:date" - ( GET ) - to get history weather of a location by ID amd Date`,
+        route8:`"/weatherByName/:name" - ( GET ) - to get weather data by simply entering name of the city as params `,
     })
  })
 
