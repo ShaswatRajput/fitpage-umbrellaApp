adb248e9ac6e8af47fc24ebb786535488a09534f
Shaswat <shaswatrajput7@gmail.com>
2023-12-14 19:35:09 +0530

    changed default route message

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,15 @@ const router = require("./routes/weather-routes")
  app.get("/",(req,res)=>{
     res.status(200).json({
         status:"successful",
-        greeting: "Hello welcome to the weather app"    
+        greeting: "Hello welcome to the weather app",
+        routes: ` "/" - Default Home Route
+        "/locations" - ( GET ) - to get all the location
+        "/locations" - ( POST ) - to add a new location ({ name, latitude, longitude})
+        "/locations/:id" - ( GET ) - to get a saved location by ID
+        "/locations/:id" - ( PUT ) - to update a saved location by ID
+        "/locations/:id" - ( DELETE ) - to delete a saved location by ID
+        "/history/:id/:date" - ( GET ) - to get history weather of a location by ID amd Date
+        "/weatherByName/:name" - ( GET ) - to get weather data by simply entering name of the city as params `  
     })
  })
 
